4f7c07fb2cedd878eaaa88f2e63b5e740c9786ac
bassrock <bassrock@users.noreply.github.com>
2015-12-14 14:56:45 -0800

    Removing unneeded variables

--- Entity/Repository/ResqueJobRepository.php
+++ Entity/Repository/ResqueJobRepository.php
@@ -14,38 +14,11 @@ use Doctrine\DBAL\Types\Type;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Query\Parameter;
 use Doctrine\ORM\Query\ResultSetMappingBuilder;
-use JMS\DiExtraBundle\Annotation as DI;
-use Symfony\Bridge\Doctrine\RegistryInterface;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use DateTime;
 use Doctrine\DBAL\Connection;
 
 
 class ResqueJobRepository extends EntityRepository {
-
-    private $dispatcher;
-    private $registry;
-
-    /**
-     * @DI\InjectParams({
-     * "dispatcher" = @DI\Inject("event_dispatcher"),
-     * })
-     * @param EventDispatcherInterface $dispatcher
-     */
-    public function setDispatcher(EventDispatcherInterface $dispatcher)
-    {
-        $this->dispatcher = $dispatcher;
-    }
-    /**
-     * @DI\InjectParams({
-     * "registry" = @DI\Inject("doctrine"),
-     * })
-     * @param RegistryInterface $registry
-     */
-    public function setRegistry(RegistryInterface $registry)
-    {
-        $this->registry = $registry;
-    }
 
     /**
      * @param $uuid
